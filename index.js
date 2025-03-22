5d0232ee07e00a1d0bbf37a1b1d0a68477d074ca
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2025-03-22 17:05:17 -0300

    feat ✨: Add donation, story and projects pages with styling

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/doacao', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'doacao.html'));
+});
+
+app.get('/historia', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'historia.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
