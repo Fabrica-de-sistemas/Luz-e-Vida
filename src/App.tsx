79026586d99016bec2da00f1c10655361bf6e128
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2025-04-23 09:52:10 -0300

    refactor: Ported Projetos page and ported updated Historia component with new images and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './app.css'
 import Layout from './partials/layout'
 import Doacoes from './pages/doacoes'
 import Historia from './pages/historia'
+import Projetos from './pages/projetos'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route index Component={Home} path="/Luz-e-Vida" />
           <Route path='/Luz-e-Vida/doacoes' Component={Doacoes} />
           <Route path='/Luz-e-Vida/historia' Component={Historia} />
+          <Route path='/Luz-e-Vida/projetos' Component={Projetos} />
           <Route path='*' Component={() => <h1 className='font-black w-full text-center h-svw text-9xl'>Página não encontrada</h1>} />
         </Route>
       </Routes>
