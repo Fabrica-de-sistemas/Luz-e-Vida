c151258eb43a93e1be1d47a10c8857ff7c6632a7
Henrique Messias Pimenta <henrique.pim2@gmail.com>
2025-04-24 19:31:20 -0300

    feat: Add Stripe integration

--- src/pages/doacoes/donate.tsx
+++ src/pages/doacoes/donate.tsx
@@ -1,11 +1,6 @@
 import doacoesImg from '../../assets/images/doacoes_image.jpeg'
 
-interface DonateProps {
-    toggleShowCheckout: () => void;
-    setValue: (value: number) => void;
-}
-
-export default function Donate({ toggleShowCheckout, setValue }: DonateProps) {
+export default function Donate() {
     return (
         <>
             {/* Doações */}
@@ -26,49 +21,49 @@ export default function Donate({ toggleShowCheckout, setValue }: DonateProps) {
 
                         {/* Grade de opções de doação */}
                         <div className="grid grid-cols-2 gap-6 w-full">
-                            {/* Opção 1: R$20,00 */}
+                            {/* Opção 1: R$10,00 */}
                             <div className="flex flex-col items-center gap-2">
-                                <button type="button"
+                                <a href='https://buy.stripe.com/test_5kA9DY5JD6QPf7ifYZ'
                                     className="flex w-full p-[10px] justify-center items-center gap-[10px] rounded-[12px] border border-[#FEFEFE] hover:bg-[#6171a3] transition-colors cursor-pointer"
-                                    onClick={() => { setValue(20); toggleShowCheckout() }} >
-                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 20,00</span>
-                                </button>
+                                    >
+                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 10,00</span>
+                                </a>
                                 <p className="w-full text-[#FEFEFE] text-sm font-normal leading-[150%]">
                                     Ajude a fornecer materiais educativos
                                 </p>
                             </div>
 
-                            {/* Opção 2: R$25,00 */}
+                            {/* Opção 2: R$20,00 */}
                             <div className="flex flex-col items-center gap-2">
-                                <button type="button"
+                                <a href='https://buy.stripe.com/test_00g3fA0pja31bV6002'
                                     className="flex w-full p-[10px] justify-center items-center gap-[10px] rounded-[12px] border border-[#FEFEFE] hover:bg-[#6171a3] transition-colors cursor-pointer"
-                                    onClick={() => { setValue(25); toggleShowCheckout() }} >
-                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 25,00</span>
-                                </button>
+                                    >
+                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 20,00</span>
+                                </a>
                                 <p className="w-[201px] text-[#FEFEFE] text-sm font-normal leading-[150%]">
                                     Contribua com alimentação para uma criança
                                 </p>
                             </div>
 
-                            {/* Opção 3: R$100,00 */}
+                            {/* Opção 3: R$50,00 */}
                             <div className="flex flex-col items-center gap-2">
-                                <button type="button"
+                                <a href='https://buy.stripe.com/test_dR6bM6fkd1wv1gs4gj'
                                     className="flex w-full p-[10px] justify-center items-center gap-[10px] rounded-[12px] border border-[#FEFEFE] hover:bg-[#6171a3] transition-colors cursor-pointer"
-                                    onClick={() => { setValue(100); toggleShowCheckout() }}>
-                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 100,00</span>
-                                </button>
+                                    >
+                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 50,00</span>
+                                </a>
                                 <p className="w-full text-[#FEFEFE] text-sm font-normal leading-[150%]">
                                     Apoie atividades culturai e esportivas
                                 </p>
                             </div>
 
-                            {/* Opção 4: R$20,00 */}
+                            {/* Opção 4: R$100,00 */}
                             <div className="flex flex-col items-center gap-2">
-                                <button type="button"
+                                <a href='https://buy.stripe.com/test_bIY17sc81ejh3oA8wA'
                                     className="flex w-full p-[10px] justify-center items-center gap-[10px] rounded-[12px] border border-[#FEFEFE] hover:bg-[#6171a3] transition-colors cursor-pointer"
-                                    onClick={() => { setValue(20); toggleShowCheckout() }} >
-                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 20,00</span>
-                                </button>
+                                    >
+                                    <span className="text-[#FEFEFE] text-xl font-medium leading-[150%]">R$ 100,00</span>
+                                </a>
                                 <p className="w-full text-[#FEFEFE] text-sm font-normal leading-[150%]">
                                     Transforme a realidade de uma família
                                 </p>
@@ -77,19 +72,10 @@ export default function Donate({ toggleShowCheckout, setValue }: DonateProps) {
 
                         {/* Campo para outros valores e botão de doação */}
                         <div className="flex flex-col gap-6">
-                            <div className="flex items-center gap-4">
-                                <div className="flex py-[10px] justify-center items-center gap-[10px] border-b border-[#FEFEFE]">
-                                    <input type="number" name="valor" min="0" placeholder="Doar outros valores"
-                                        className="bg-transparent text-white placeholder:text-[rgba(254,254,254,0.30)] placeholder:text-xl placeholder:font-medium focus:outline-none w-48"
-                                        onChange={(e) => setValue(Number(e.target.value))}
-                                    />
-                                </div>
-                            </div>
 
                             {/* Botão de doação */}
-                            <form className="w-full" hx-post="/doacoes" hx-trigger="submit" hx-boost="true" hx-swap="innerHTML"
-                                hx-push-url="checkout" hx-target="body" encType="multipart/form-data">
-                                <button type="submit" onClick={toggleShowCheckout}
+                            <form className="w-full" hx-post="/doacoes" encType="multipart/form-data">
+                                <button type="submit" onClick={() => { }}
                                     className="flex px-5 py-[10px] justify-center items-center gap-[10px] rounded-[12px] bg-[#FAB515] shadow-[0px_4px_4px_0px_rgba(250,181,21,0.25)] text-[#FEFEFE] text-xl font-medium leading-[150%]">
                                     Quero Doar Agora
                                 </button>
